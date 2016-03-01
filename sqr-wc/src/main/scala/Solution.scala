20b85d9637c0da3150acb420436b403344f615ce
Bora Tunca <bora@soundcloud.com>
2016-03-02 00:21:27 +0100

    add sgr tetris

--- sqr-wc/src/main/scala/Solution.scala
+++ sqr-wc/src/main/scala/Solution.scala
@@ -1,7 +1,36 @@
 package square
 
+case class SquarePosition(x: Float, y: Float, dimension: Float)
+
 object Solution extends App {
 
-  println("hello square")
-  
+  def drop(x: Float, dimension: Float, currentState: List[SquarePosition]): List[SquarePosition] = {
+
+    val occupyingSqs = currentState.filter {
+      sqPos =>
+        (
+          (x >= sqPos.x && x < sqPos.x + sqPos.dimension) ||
+            (x + dimension >= sqPos.x && x + dimension < sqPos.x + sqPos.dimension)
+          ) ||
+          (
+            (sqPos.x >= x && sqPos.x < x + dimension) ||
+              (sqPos.x + sqPos.dimension >= x && sqPos.x + sqPos.dimension < x + dimension)
+            )
+
+    }
+
+    occupyingSqs match {
+      case Nil =>
+        val newIncomingPos = SquarePosition(x, 0, dimension)
+        newIncomingPos :: currentState
+      case _ =>
+        val sorted = occupyingSqs.sortWith {
+          case (sq1, sq2) =>
+            (sq1.y + sq1.dimension) > (sq2.y + sq2.dimension)
+        }
+        val newYPos = sorted.head.y + sorted.head.dimension
+        val newIncomingPos = SquarePosition(x, newYPos, dimension)
+        newIncomingPos :: currentState
+    }
+  }
 }
--- sqr-wc/src/test/scala/SolutionSpec.scala
+++ sqr-wc/src/test/scala/SolutionSpec.scala
@@ -4,8 +4,55 @@ import org.specs2.mutable.Specification
 
 class SolutionSpec extends Specification {
 
-  "first test" >> {
-    "hello" ==== "world"
+  "state is empty" >> {
+    val expected = List(SquarePosition(1f,0f,2f))
+    Solution.drop(1 ,2, List.empty) ==== expected
   }
-    
+
+  "2 boxes don't collide" >> {
+    val actual = Solution.drop(3.1f ,2, List(SquarePosition(1f,0f,2f)))
+    actual must haveSize(2)
+    actual must contain(SquarePosition(1f,0f,2f),
+      SquarePosition(3.1f,0f,2f))
+  }
+
+  "2 boxes exactly collide" >> {
+    val currentState = List(SquarePosition(1f,0f,2f))
+    val actual = Solution.drop(1f ,2f, currentState)
+
+    actual must haveSize(2)
+    actual must contain(
+      SquarePosition(1f,0f,2f),
+      SquarePosition(1f,2f,2f))
+  }
+
+  "2 boxes on top of misaligned" >> {
+    val currentState = List(SquarePosition(1f,0f,2f))
+    val actual = Solution.drop(1.5f ,2f, currentState)
+
+    actual must haveSize(2)
+    actual must contain(
+      SquarePosition(1f,0f,2f),
+      SquarePosition(1.5f,2f,2f))
+  }
+
+  "2 boxes on top of misaligned - 2" >> {
+    val currentState = List(SquarePosition(1f,0f,2f))
+    val actual = Solution.drop(0.5f ,2f, currentState)
+
+    actual must haveSize(2)
+    actual must contain(
+      SquarePosition(1f,0f,2f),
+      SquarePosition(0.5f,2f,2f))
+  }
+
+  "2 boxes on top of misaligned incoming box is too large" >> {
+    val currentState = List(SquarePosition(1f,0f,2f))
+    val actual = Solution.drop(0.5f ,4f, currentState)
+
+    actual must haveSize(2)
+    actual must contain(
+      SquarePosition(1f,0f,2f),
+      SquarePosition(0.5f,2f,4f))
+  }
 }
